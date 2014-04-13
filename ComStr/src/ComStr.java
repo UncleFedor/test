29e06b0af95a861352e9406229da3a9e626317f4
UncleFedor <o.fedorov@outlook.com>
2014-04-13 16:34:59 +0400

    урок первый

---
+++ ComStr/src/ComStr.java
@@ -0,0 +1,10 @@
+/**
+ * Created by oleg on 13.04.14.
+ */
+public class ComStr {
+public static void main(String[] args) {
+    for(String a: args) System.out.println(a);
+    System.out.println("Всего аргументов: "+args.length);
+
+}
+}
