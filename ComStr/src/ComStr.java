4c411699f94d75d24517de293df5a790842964b7
Oleg Fedorov <oleg@fedorov.msk.ru>
2014-04-15 14:39:42 +0400

    урок исключения

--- ComStr/src/ComStr.java
+++ ComStr/src/ComStr.java
@@ -3,7 +3,16 @@
  */
 public class ComStr {
 public static void main(String[] args) {
-    for(String a: args) System.out.print(a+" ");
+    int a,b;
+    a=6; b=0;
+    try {
+        a = a / b;
+    }
+    catch (ArithmeticException e){
+        System.out.println("Деление на ноль.");
+        a=0;
+    }
+    for(String s: args) System.out.print(s+" ");
     System.out.println();
     System.out.println("Всего аргументов: "+args.length);
     }
